7a5043e905186b8cad687a1daebfdf810526cbf2
liyy2 <liyy2@shanghaitech.edu.cn>
2023-08-16 11:59:09 -0400

    refactor code

--- code/process.py
+++ code/process.py
@@ -213,17 +213,22 @@ def fit_model(X, y, model = 'XGBoost', grid_search_cv = 0, random_state = 42,
 
     from scipy.stats import pearsonr
     from scipy.stats import spearmanr
+    from metrics import mean_bias_deviation
     # Assuming y_test are your ground truth values and predictions are your model's predictions
     correlation, _ = pearsonr(y_test, predictions)
     rmse_error = np.sqrt(mean_squared_error(y_test, predictions))
     mae_error = mean_absolute_error(y_test, predictions)
     rho, _ = spearmanr(y_test, predictions)
+    mbd = mean_bias_deviation(y_test, predictions)
     print('Pearson correlation: %.3f' % correlation)
     print('Spearman correlation: %.3f' % rho)
     print('RMSE: %.3f' % rmse_error )
     print('MAE: %.3f' % mae_error)
+    print('MBD: %.3f' % mbd)
     return model, X_train, X_test, y_train, y_test, predictions, \
-    {'Pearson Correlation': correlation, 'Spearman Correlation': rho, 'RMSE': rmse_error, 'MAE': mae_error, 'Random_state': random_state}, save 
+    {'Pearson Correlation': correlation, 'Spearman Correlation': rho, 
+    'RMSE': rmse_error, 'MAE': mae_error, 
+    'MBD': mbd, 'Random_state': random_state}, save 
 
     # Now 'predictions' will hold the predicted 'Age_death' for the test set.
     
@@ -351,48 +356,3 @@ def train_val_test_split(X, y, test_size = 0.2, val_size = 0.25, random_state = 
     X_train, X_val, y_train, y_val \
     = train_test_split(X_train, y_train, test_size=val_size, random_state=random_state) # 0.25 x 0.8 = 0.2
     return X_train, X_val, X_test, y_train, y_val, y_test
-
-def parse_argument():
-    # argumant parser
-    parser = argparse.ArgumentParser(description='Process data from a given csv file and return a dataframe')
-    parser.add_argument('-g', '--gene_num', help='Number of genes', type=int, default=-1)
-    parser.add_argument('-d', '--dataset_id', help='Dataset id', default='24')
-    parser.add_argument('-p', '--partition', help = 'partition',type=int,default=1)
-    parser.add_argument('-i', '--index', help = 'index of the partition',type=int, default=0)
-    parser.add_argument('-s', '--stratify', help = 'Stratified sampling', type=int, default=0)
-    parser.add_argument('-c', '--cross_val', help = 'Cross Validation Fold', type = int, default=3)
-    parser.add_argument('-n', '--num_runs', help = 'Number of different seeds', type = int, default=10)
-    parser.add_argument('-A', '--AD_Model', help= 'Train on control and predict on AD', type = int, default=0)
-    parser.add_argument('--save_intermediate', help='save intermediate results', type = int, default=1)
-    parser.add_argument('--shap', help='perform shap', type = int, default=1)
-    parser.add_argument('--feature_importance', help='record feature importance', type = int, default=1)
-    args = parser.parse_args()
-    return args
-
-
-if __name__ == '__main__':
-    args = parse_argument()
-    celltype_names =  [
- 'L2.3.IT',
- 'Oligo',
- 'Astro',
- 'L4.IT',
- 'OPC', 'Chandelier__Pvalb', 'Sst__Sst.Chodl' , 'L5.IT', 'L6.IT', 'Vip', 'Micro.PVM']
-
-    # celltype_names = ['L2.3.IT']
-    intersect_of_all = get_index_intersect(celltype_names, dataset_id = args.dataset_id)
-    celltype_names_subsetted = [celltype_names[i] for i in range(args.index, len(celltype_names), args.partition)]
-   
-    rets = predict_with_different_seeds(celltype_names_subsetted,
-    gene_num = args.gene_num, dataset_id=args.dataset_id, 
-    fixed_index = intersect_of_all, 
-    AD_model = False, stratify = args.stratify, 
-    grid_search_cv = args.cross_val, 
-    num_splits = args.num_runs,
-    AD_Model = args.AD_Model, 
-    save_intermediate = args.save_intermediate, 
-    shap = args.shap, 
-    feature_importance = args.feature_importance)
-
-    stratify = 'stratified' if args.stratify else 'random'
-    rets.to_csv(f'results_{args.index}_outof_{args.partition}_gene_num_{args.gene_num}_dataset_{args.dataset_id}_{stratify}.csv')
